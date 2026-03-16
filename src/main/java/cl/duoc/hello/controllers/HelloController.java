545af2789e87cfb0b5d83395116034183ba05e17
SB- Alumno <SB-Alumno@duoc.acad>
2026-03-16 17:12:52 -0300

    First commit

---
+++ src/main/java/cl/duoc/hello/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package cl.duoc.hello.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(@RequestParam(required = false) String value) {
+        String name = "World";
+        if (value!=null) name = value;
+        return String.format("Hello, %s!", name);
+    }
+}
---
+++ src/test/java/cl/duoc/hello/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
